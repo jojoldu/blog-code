1c5315cdef7ed8ced921b10f7a34af0cc38e4041
이동욱 <jojoldu@gmail.com>
2016-09-04 21:20:23 +0900

    JpaTest 문서 정리 중

---
+++ springboot-test/src/test/java/com/example/RestTest.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by jojoldu@gmail.com on 2016-09-03.
+ * Blog : http://jojoldu.tistory.com
+ * Github : http://github.com/jojoldu
+ */
+
+@RunWith(SpringRunner.class)
+@RestClientTest()
+public class RestTest {
+}
