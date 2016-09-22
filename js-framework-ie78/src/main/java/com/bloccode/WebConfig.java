3e2c9f2dc86a275a3edeeb1aed5a3b0cdbad5ca2
이동욱 <jojoldu@zuminternet.com>
2016-09-22 09:15:39 +0900

    기본 Java 설정 추가

---
+++ js-framework-ie78/src/main/java/com/bloccode/WebConfig.java
@@ -0,0 +1,21 @@
+package com.bloccode;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by jojoldu@gmail.com on 2016-09-21.
+ * Blog : http://jojoldu.tistory.com
+ * Github : http://github.com/jojoldu
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+    }
+}
---
+++ js-framework-ie78/src/main/resources/templates/index.ftl
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>IE에서도 모던하게 개발하자</title>
+</head>
+<body>
+    <script type="text/javascript" src="/build/js/lib.js"></script>
+    <script type="text/javascript" src="/build/js/service.js"></script>
+</body>
+</html>
