659141e4bccc110e1a8427ae7487feea17dde7c3
이동욱 <jojoldu@gmail.com>
2016-09-26 23:16:38 +0900

    grunt file 작성중

--- js-framework-ie78/Gruntfile.js
+++ js-framework-ie78/Gruntfile.js
@@ -8,10 +8,10 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-        'pkg' : grunt.file.readJSON('package.json'),
+        pkg : grunt.file.readJSON('package.json'),
 
-        // build 전 build된 resource들 정리
-        'clean': [
+        // build 전 이전 resource들 정리
+        clean: [
             'src/main/resources/static/build/*',
             'src/main/resources/static/js/templates.js'
         ],
@@ -27,17 +27,25 @@ module.exports = function(grunt) {
         },
         //concat 설정
         concat: {
-            basic: {
-                src: ['public/js/common/util.js', 'public/js/app.js', 'public/js/lib/.js', 'public/js/ctrl/.js'], //concat 타겟 설정(앞에서부터 순서대로 합쳐진다.)
-                dest: 'public/build/result.js' //concat 결과 파일
+            lib : {
+                src : [ //concat 타겟 설정(앞에서부터 순서대로 합쳐진다.)
+                    'node_modules/backbone/backbone-min.js',
+                    'node_modules/backbone/node_modules/underscore-min.js',
+                    'node_modules/jquery.1/node_modules/jquery/dist/jquery.min.js', // IE 하위버전 호환을 위해 jquery는 1.x 버전을 사용
+                    'node_modules/json2/lib/JSON2/static/json2.js',
+                    'node_modules/handlebars/dist/handlebars.min.js',
+                    'node_modules/requirejs/require.js'
+                ],
+                dest : 'src/main/resources/static/build/lib.js' //concat 결과 파일
             }
         }
     });
 
     // Load the plugin that provides the "uglify", "concat" tasks.
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
 
     // Default task(s).
-    grunt.registerTask('default', ['concat', 'uglify']); //grunt 명령어로 실행할 작업
+    grunt.registerTask('default', ['clean', 'concat', 'uglify']); //grunt 명령어로 실행할 작업
 };
