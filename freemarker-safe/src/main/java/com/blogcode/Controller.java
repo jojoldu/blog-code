87794dd2b6860e314fbf133be97cae6fbf57199a
이동욱 <jojoldu@zuminternet.com>
2016-10-06 15:06:52 +0900

    프로젝트 세팅

---
+++ freemarker-safe/src/main/java/com/blogcode/Controller.java
@@ -0,0 +1,10 @@
+package com.blogcode;
+
+/**
+ * Created by jojoldu@gmail.com on 2016-10-06.
+ * Blog : http://jojoldu.tistory.com
+ * Github : http://github.com/jojoldu
+ */
+
+public class Controller {
+}
