fb03adb4c9d15ddfa9dc9fc4cb5024007e332a46
이동욱 <jojoldu@zuminternet.com>
2016-10-12 08:54:45 +0900

    backbone.js collection.js 생성

---
+++ js-framework-ie78/src/main/resources/static/js/member/MemberCollection.js
@@ -0,0 +1,12 @@
+/**
+ * Created by jojoldu@gmail.com on 2016-10-12.
+ * Blog : http://jojoldu.tistory.com
+ * Github : http://github.com/jojoldu
+ */
+
+define(['member/MemberModel'],
+function (MemberModel) {
+    return Backbone.collection.extend({
+        model : MemberModel
+    });
+});
