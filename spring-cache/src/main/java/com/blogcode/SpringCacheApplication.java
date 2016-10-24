0a9e4b457e4554b397731ca56733960eb500886e
이동욱 <jojoldu@gmail.com>
2016-10-24 18:14:46 +0900

    프로젝트 생성

---
+++ spring-cache/src/main/java/com/blogcode/SpringCacheApplication.java
@@ -0,0 +1,12 @@
+package com.blogcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCacheApplication.class, args);
+	}
+}
