fceeb2c5a921c76813617f007949b9874c7c9d37
이동욱 <jojoldu@gmail.com>
2016-10-31 22:05:30 +0900

    [server/nginx-cache] 샘플 프로젝트 생성 (img/gif)

---
+++ server/nginx-cache/src/main/java/com/blogcode/Application.java
@@ -0,0 +1,20 @@
+package com.blogcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
---
+++ server/nginx-cache/src/main/resources/templates/index.ftl
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Nginx Cache by Blog Code</title>
+</head>
+<body>
+    <h1>검색하세요!</h1>
+    <img src="/images/doodle.gif" alt="검색하세요">
+    <br/>
+    <img src="/images/zum.png" alt="zum.com">
+</body>
+</html>
