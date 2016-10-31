f6e79f6a0e4d9e447287c9e96619f89ef619ae60
이동욱 <jojoldu@gmail.com>
2016-10-31 16:35:17 +0900

    [spring/cache] 현재 cache manager 체크 기능 추가

---
+++ spring-cache/src/main/java/com/blogcode/CacheManagerCheck.java
@@ -0,0 +1,33 @@
+package com.blogcode;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.cache.CacheManager;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jojoldu@gmail.com on 2016-10-31.
+ * Blog : http://jojoldu.tistory.com
+ * Github : http://github.com/jojoldu
+ */
+
+@Component
+public class CacheManagerCheck implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(CacheManagerCheck.class);
+
+    private final CacheManager cacheManager;
+
+    public CacheManagerCheck(CacheManager cacheManager) {
+        this.cacheManager = cacheManager;
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        logger.info("\n\n" + "=========================================================\n"
+                + "Using cache manager: " + this.cacheManager.getClass().getName() + "\n"
+                + "=========================================================\n\n");
+    }
+
+}
