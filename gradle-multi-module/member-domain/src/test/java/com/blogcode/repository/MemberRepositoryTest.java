ec8ed8e3f25bd7fd21f5182f79d87a79c4ef9162
이동욱 <jojoldu@gmail.com>
2017-02-12 23:16:59 +0900

    [gradle-multi-module] member-domain 간단 테스트 통

--- gradle-multi-module/member-domain/src/test/java/com/blogcode/repository/MemberRepositoryTest.java
+++ gradle-multi-module/member-domain/src/test/java/com/blogcode/repository/MemberRepositoryTest.java
@@ -1,10 +1,15 @@
 package com.blogcode.repository;
 
+import com.blogcode.model.Member;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Created by jojoldu@gmail.com on 2017. 2. 12.
@@ -19,7 +24,10 @@ public class MemberRepositoryTest {
     @Autowired
     private MemberRepository memberRepository;
 
+    @Test
     public void save() {
-        Mem
+        memberRepository.save(new Member("jojoldu", "jojoldu@gmail.com"));
+        Member saved = memberRepository.findAll().get(0);
+        assertThat(saved.getName(), is("jojoldu"));
     }
 }
