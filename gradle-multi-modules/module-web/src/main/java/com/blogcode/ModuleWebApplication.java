816a593ce78ed6f0262d0758290868e670522363
이동욱 <jojoldu@gmail.com>
2017-02-17 19:35:05 +0900

    [gradle-multi-module] cradle dependency management 가 필요했구나 ㅜㅜ..

--- gradle-multi-modules/module-web/src/main/java/com/blogcode/ModuleWebApplication.java
+++ gradle-multi-modules/module-web/src/main/java/com/blogcode/ModuleWebApplication.java
@@ -9,4 +9,5 @@ public class ModuleWebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ModuleWebApplication.class, args);
 	}
+
 }
---
+++ gradle-multi-modules/module-web/src/main/java/com/blogcode/member/MemberController.java
@@ -0,0 +1,20 @@
+package com.blogcode.member;
+
+import com.blogcode.domain.Member;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by jojoldu@gmail.com on 2017. 2. 17.
+ * Blog : http://jojoldu.tistory.com
+ * Github : http://github.com/jojoldu
+ */
+
+@RestController
+public class MemberController {
+
+    @GetMapping("/")
+    public Member get() {
+        return new Member("jojoldu", "jojoldu@gmail.com");
+    }
+}
