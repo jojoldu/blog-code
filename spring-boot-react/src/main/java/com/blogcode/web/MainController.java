981a52e0c9684ecc9cf616f7073373eb609a56ed
이동욱 <jojoldu@gmail.com>
2017-02-19 21:32:56 +0900

    [spring-boot-react] 프로젝트 생성

---
+++ spring-boot-react/src/main/java/com/blogcode/web/MainController.java
@@ -0,0 +1,15 @@
+package com.blogcode.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jojoldu@gmail.com on 2017. 2. 19.
+ * Blog : http://jojoldu.tistory.com
+ * Github : http://github.com/jojoldu
+ */
+
+@Controller
+public class MainController {
+
+}
