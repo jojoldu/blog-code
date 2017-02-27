fa1546b02c486554ef445ca37aefc8928f752b68
이동욱 <jojoldu@gmail.com>
2017-02-28 08:34:00 +0900

    [spring-validation] index.html은 자동으로 루트 path에 잡히네??

--- spring-validation/README.md
+++ spring-validation/README.md
@@ -1,1 +1,3 @@
 # Validation 공통모듈 만들기
+
+* resources/static/index.html을 생성하면 "/" 접속시 자동으로 index.html을 호출한다
--- spring-validation/src/main/java/com/blogcode/domain/web/MainController.java
+++ spring-validation/src/main/java/com/blogcode/domain/web/MainController.java
@@ -4,10 +4,10 @@ import com.blogcode.domain.member.MemberService;
 import com.blogcode.domain.member.dto.MemberRequestDto;
 import com.blogcode.domain.member.dto.ValidTestDto;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
@@ -17,7 +17,7 @@ import javax.validation.Valid;
  * Github : http://github.com/jojoldu
  */
 
-@Controller
+@RestController
 public class MainController {
 
     private MemberService memberService;
@@ -26,19 +26,12 @@ public class MainController {
         this.memberService = memberService;
     }
 
-    @GetMapping("/")
-    public String index() {
-        return "index";
-    }
-
     @PostMapping("/member")
-    @ResponseBody
     public Long saveMember(@RequestBody @Valid MemberRequestDto memberRequestDto) {
         return memberService.save(memberRequestDto);
     }
 
     @PostMapping("/test")
-    @ResponseBody
     public ValidTestDto validTest(@Valid ValidTestDto validTestDto){
         return validTestDto;
     }
