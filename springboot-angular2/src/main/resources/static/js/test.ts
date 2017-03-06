e8031ca02e58e176702db6fe19ec50fd83adcdc5
이동욱 <jojoldu@gmail.com>
2017-03-06 23:07:17 +0900

    타입스크립트 예제 실행

---
+++ springboot-angular2/src/main/resources/static/js/test.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by jojoldu@gmail.com on 2017. 3. 6.
+ * Blog : http://jojoldu.tistory.com
+ * Github : http://github.com/jojoldu
+ */
+let arr: string[] = [];
+arr.push('a');
+arr.push('b');
+arr.push('c');
+console.log(arr[0]);
