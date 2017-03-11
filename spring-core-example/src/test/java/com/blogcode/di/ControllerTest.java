1ffe306134401c5a584a0ba12dfbb98c46f583f8
이동욱 <jojoldu@gmail.com>
2017-03-11 20:03:03 +0900

    [spring-core-example] step1 테스트 코드 완성

--- spring-core-example/src/test/java/com/blogcode/di/ControllerTest.java
+++ spring-core-example/src/test/java/com/blogcode/di/ControllerTest.java
@@ -1,11 +1,28 @@
 package com.blogcode.di;
 
+import com.blogcode.di.step1.Controller;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * Created by jojoldu@gmail.com on 2017. 3. 11.
  * Blog : http://jojoldu.tistory.com
  * Github : http://github.com/jojoldu
  */
 
-
 public class ControllerTest {
+
+    @Test
+    public void step1테스트() {
+
+        //given
+        Controller controller = new Controller();
+
+        //do
+        assertThat(controller.getMember("html"), is("html : id: 1 name: 이동욱 email: jojoldu@gmail.com"));
+        assertThat(controller.getMember("json"), is("json : id: 1 name: 이동욱 email: jojoldu@gmail.com"));
+        assertThat(controller.getMember("xml"), is("xml : id: 1 name: 이동욱 email: jojoldu@gmail.com"));
+    }
 }
