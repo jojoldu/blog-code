fd739778d35fe99a2afcb06d52cdc0b6f716a201
이동욱 <jojoldu@gmail.com>
2017-03-12 23:38:53 +0900

    [springboot-angular2] tsconfig build 설적 적용

---
+++ springboot-angular2/src/main/resources/static/js/app/main/main.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by jojoldu@gmail.com on 2017. 3. 12.
+ * Blog : http://jojoldu.tistory.com
+ * Github : http://github.com/jojoldu
+ */
+
+let main: string[] = [];
+main.push('a');
+main.push('b');
+main.push('c');
+console.log(main[0]);
---
+++ springboot-angular2/src/main/resources/static/js/app/test.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by jojoldu@gmail.com on 2017. 3. 6.
+ * Blog : http://jojoldu.tistory.com
+ * Github : http://github.com/jojoldu
+ */
+let array: string[] = [];
+array.push('a');
+array.push('b');
+array.push('c');
+console.log(array[0]);
