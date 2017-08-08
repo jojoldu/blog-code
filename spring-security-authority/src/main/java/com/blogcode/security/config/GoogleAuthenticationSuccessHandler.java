dd422e226a80f7dfe349ba31cc78d0821e9f6cf3
이동욱 <jojoldu@gmail.com>
2017-08-09 08:59:14 +0900

    h2 / MySQL 세션 스키마 적용

--- spring-security-authority/src/main/java/com/blogcode/security/config/GoogleAuthenticationSuccessHandler.java
+++ spring-security-authority/src/main/java/com/blogcode/security/config/GoogleAuthenticationSuccessHandler.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
@@ -18,8 +19,15 @@ import java.io.IOException;
 @Component
 public class GoogleAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
+    private HttpSession httpSession;
+
+    public GoogleAuthenticationSuccessHandler(HttpSession httpSession) {
+        this.httpSession = httpSession;
+    }
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        httpSession.setAttribute("authUser", "authUser");
         response.sendRedirect("/");
     }
 }
