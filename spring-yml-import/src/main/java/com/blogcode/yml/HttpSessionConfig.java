9685f7d43dde13a49af117e8c64dc97ebcf14736
이동욱 <jojoldu@gmail.com>
2017-08-16 20:48:28 +0900

    [spring-yml-import] 세션 적용전

---
+++ spring-yml-import/src/main/java/com/blogcode/yml/HttpSessionConfig.java
@@ -0,0 +1,13 @@
+package com.blogcode.yml;
+
+//import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+
+/**
+ * Created by jojoldu@gmail.com on 2017. 8. 8.
+ * Blog : http://jojoldu.tistory.com
+ * Github : https://github.com/jojoldu
+ */
+
+//@EnableJdbcHttpSession
+public class HttpSessionConfig {
+}
---
+++ spring-yml-import/src/main/java/com/blogcode/yml/OAuthConfig.java
@@ -0,0 +1,92 @@
+package com.blogcode.yml;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.Filter;
+
+/**
+ * Created by jojoldu@gmail.com on 2017. 8. 16.
+ * Blog : http://jojoldu.tistory.com
+ * Github : https://github.com/jojoldu
+ */
+
+@Configuration
+@EnableOAuth2Client
+public class OAuthConfig {
+
+    private final OAuth2ClientContext oauth2ClientContext;
+
+    public OAuthConfig(OAuth2ClientContext oauth2ClientContext) {
+        this.oauth2ClientContext = oauth2ClientContext;
+    }
+
+    @Bean
+    public Filter ssoFilter() {
+        OAuth2ClientAuthenticationProcessingFilter oauth2Filter = new OAuth2ClientAuthenticationProcessingFilter("/login");
+        OAuth2RestTemplate oAuth2RestTemplate = new OAuth2RestTemplate(googleClient(), oauth2ClientContext);
+        oauth2Filter.setRestTemplate(oAuth2RestTemplate);
+        oauth2Filter.setTokenServices(new UserInfoTokenServices(googleResource().getUserInfoUri(), googleClient().getClientId()));
+        oauth2Filter.setAuthenticationSuccessHandler(successHandler()); // 인증 성공시 진행될 Handler 등록
+        return oauth2Filter;
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler successHandler(){
+        return (request, response, authentication) -> {
+            System.out.println("성공!");
+            response.sendRedirect("/");
+        };
+    }
+
+    @Bean
+    @ConfigurationProperties("google.client")
+    public OAuth2ProtectedResourceDetails googleClient() {
+        System.out.println("google client Call");
+        return new AuthorizationCodeResourceDetails();
+    }
+
+    @Bean
+    @ConfigurationProperties("google.resource")
+    public ResourceServerProperties googleResource() {
+        System.out.println("google resource Call");
+        return new ResourceServerProperties();
+    }
+
+    @Bean
+    public FilterRegistrationBean oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(filter);
+        registration.setOrder(-100);
+        return registration;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+
+        System.out.println("google yml Call");
+
+        YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
+        yaml.setResources(new ClassPathResource("google.yml"));
+
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        propertySourcesPlaceholderConfigurer.setProperties(yaml.getObject());
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
