751753f2e5c7e05b516d4f21d29e0475a71fb332
이동욱 <jojoldu@gmail.com>
2017-10-17 09:00:57 +0900

    [intellij-h2-client] 서버 콘솔 추가

--- intellij-h2-client/src/main/java/com/jojoldu/h2/domain/H2ServerConfiguration.java
+++ intellij-h2-client/src/main/java/com/jojoldu/h2/domain/H2ServerConfiguration.java
@@ -19,7 +19,12 @@ public class H2ServerConfiguration {
 
     @Bean
     public Server h2TcpServer() throws SQLException {
-        return Server.createTcpServer()
-                .start();
+        Server h2Server = Server.createTcpServer().start();
+        if (h2Server.isRunning(true)) {
+            System.out.println("H2 server was started and is running.");
+            return h2Server;
+        } else {
+            throw new RuntimeException("Could not start H2 server.");
+        }
     }
 }
