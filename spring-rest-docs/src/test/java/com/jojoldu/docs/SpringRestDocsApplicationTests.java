6d28a5b7898a1f43e0671d41db30074cb5ad7de1
jojoldu <jojoldu@gmail.com>
2018-04-20 09:21:57 +0900

    [rest-docs] 시작

---
+++ spring-rest-docs/src/test/java/com/jojoldu/docs/SpringRestDocsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jojoldu.docs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRestDocsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
