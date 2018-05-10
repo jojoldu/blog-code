66bebd2f889d8b52a3a29f0bdaf0c2babbd0f667
jojoldu <jojoldu@gmail.com>
2018-05-10 21:57:19 +0900

    샘플 테스트 코드 완성

--- springboot-jpa-id/src/test/java/com/jojoldu/blogcode/springbootjpaid/ApplicationTests.java
+++ springboot-jpa-id/src/test/java/com/jojoldu/blogcode/springbootjpaid/ApplicationTests.java
@@ -1,10 +1,13 @@
 package com.jojoldu.blogcode.springbootjpaid;
 
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,14 +19,24 @@ public class ApplicationTests {
     @Autowired
     private MemberRepository memberRepository;
 
+    @After
+    public void tearDown() throws Exception {
+        bookRepository.deleteAllInBatch();
+        memberRepository.deleteAllInBatch();
+    }
 
     @Test
     public void name() {
         //given
+        bookRepository.save(new Book("book1"));
+        memberRepository.save(new Member("member1"));
 
         //when
+        Book book = bookRepository.findAll().get(0);
+        Member member = memberRepository.findAll().get(0);
 
         //then
-
+        assertThat(book.getId()).isEqualTo(1L);
+        assertThat(member.getId()).isEqualTo(1L);
     }
 }
