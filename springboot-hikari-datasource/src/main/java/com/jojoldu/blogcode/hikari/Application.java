5ebfd46866e397cc78675f55493593c9e4334e58
jojoldu <jojoldu@gmail.com>
2018-05-18 09:01:42 +0900

    [히카리 datasource] 재료 코드 완성

--- springboot-hikari-datasource/src/main/java/com/jojoldu/blogcode/hikari/Application.java
+++ springboot-hikari-datasource/src/main/java/com/jojoldu/blogcode/hikari/Application.java
@@ -1,10 +1,34 @@
 package com.jojoldu.blogcode.hikari;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Application {
+
+    @Bean(name = "dataSource")
+    @Primary
+    @ConfigurationProperties("spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Bean(name = "secondDataSource")
+    @ConfigurationProperties("second.datasource.hikari")
+    public DataSource secondDataSource() {
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- springboot-hikari-datasource/src/test/java/com/jojoldu/blogcode/hikari/ApplicationTests.java
+++ springboot-hikari-datasource/src/test/java/com/jojoldu/blogcode/hikari/ApplicationTests.java
@@ -2,15 +2,22 @@ package com.jojoldu.blogcode.hikari;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
 
+    @Autowired
+    DataSource dataSource;
+
     @Test
     public void contextLoads() {
+        System.out.println(dataSource);
     }
 
 }
