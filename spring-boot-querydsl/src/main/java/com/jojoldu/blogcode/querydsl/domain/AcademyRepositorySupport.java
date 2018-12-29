ea564baa469cfa1a74eee58a6c70e599f80d2739
jojoldu <jojoldu@gmail.com>
2018-12-29 13:04:15 +0900

    기본기능 완성

---
+++ spring-boot-querydsl/src/main/java/com/jojoldu/blogcode/querydsl/domain/AcademyRepositorySupport.java
@@ -0,0 +1,32 @@
+package com.jojoldu.blogcode.querydsl.domain;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static com.jojoldu.blogcode.querydsl.domain.QAcademy.academy;
+
+/**
+ * Created by jojoldu@gmail.com on 2018-12-29
+ * Blog : http://jojoldu.tistory.com
+ * Github : https://github.com/jojoldu
+ */
+
+@Repository
+public class AcademyRepositorySupport extends QuerydslRepositorySupport {
+    private final JPAQueryFactory queryFactory;
+
+    public AcademyRepositorySupport(JPAQueryFactory queryFactory) {
+        super(Academy.class);
+        this.queryFactory = queryFactory;
+    }
+
+    public List<Academy> findByName(String name) {
+        return queryFactory.selectFrom(academy)
+                .where(academy.name.eq(name))
+                .fetch();
+    }
+
+}
---
+++ spring-boot-querydsl/src/test/java/com/jojoldu/blogcode/querydsl/BasicTest.java
@@ -0,0 +1,54 @@
+package com.jojoldu.blogcode.querydsl;
+
+import com.jojoldu.blogcode.querydsl.domain.Academy;
+import com.jojoldu.blogcode.querydsl.domain.AcademyRepository;
+import com.jojoldu.blogcode.querydsl.domain.AcademyRepositorySupport;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+
+/**
+ * Created by jojoldu@gmail.com on 2018-12-29
+ * Blog : http://jojoldu.tistory.com
+ * Github : https://github.com/jojoldu
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasicTest {
+    
+    @Autowired
+    private AcademyRepository academyRepository;
+    
+    @Autowired
+    private AcademyRepositorySupport academyRepositorySupport;
+
+    @After
+    public void tearDown() throws Exception {
+        academyRepository.deleteAllInBatch();
+    }
+
+    @Test
+    public void querydsl_기본_기능_확인() {
+        //given
+        String name = "jojoldu";
+        String address = "jojoldu@gmail.com";
+        academyRepository.save(new Academy(name, address));
+
+        //when
+        List<Academy> result = academyRepositorySupport.findByName(name);
+
+        //then
+        assertThat(result.size(), is(1));
+        assertThat(result.get(0).getAddress(), is(address));
+    }
+}
