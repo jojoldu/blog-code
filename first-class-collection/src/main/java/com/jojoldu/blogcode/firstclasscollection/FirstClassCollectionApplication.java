a787188138b8eddc4b72e5734dcef2db7e74aa13
이동욱 <jojoldu@gmail.com>
2019-03-25 19:15:59 +0900

    일급 컬렉션 작성중

---
+++ first-class-collection/src/main/java/com/jojoldu/blogcode/firstclasscollection/FirstClassCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.blogcode.firstclasscollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstClassCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstClassCollectionApplication.class, args);
+    }
+
+}
