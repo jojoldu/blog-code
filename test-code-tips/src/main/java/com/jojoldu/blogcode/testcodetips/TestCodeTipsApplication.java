0c041812937cb7f21dce500739da2212bc774599
이동욱 <jojoldu@gmail.com>
2019-04-26 18:20:03 +0900

    테스트 코드 팁 정리중

---
+++ test-code-tips/src/main/java/com/jojoldu/blogcode/testcodetips/TestCodeTipsApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.blogcode.testcodetips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCodeTipsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCodeTipsApplication.class, args);
+    }
+
+}
