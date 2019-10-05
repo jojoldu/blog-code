340aa07c3f2ef4a365f41ee3f85a98f60563102d
이동욱 <jojoldu@gmail.com>
2019-10-05 12:07:55 +0900

    sqs 추상 메세지 프로젝트 생성

---
+++ spring-boot-sqs/src/main/java/com/jojoldu/blogcode/springbootsqs/SpringBootSqsApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.blogcode.springbootsqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSqsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSqsApplication.class, args);
+    }
+
+}
