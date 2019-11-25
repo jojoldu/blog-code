8d23f9fdaf78cbe0c2730c503a2eb15607712049
이동욱 <jojoldu@gmail.com>
2019-11-25 17:53:07 +0900

    1,2,3 테스트 다 통과하니 File Download도 추가해봐야겠다.

---
+++ spring-boot-tips/src/test/java/com/jojoldu/blogcode/springboot/tips/WebMvcConfig3.java
@@ -0,0 +1,36 @@
+package com.jojoldu.blogcode.springboot.tips;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jojoldu.blogcode.springboot.tips.web.config.HtmlCharacterEscapes;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * Created by jojoldu@gmail.com on 25/11/2019
+ * Blog : http://jojoldu.tistory.com
+ * Github : http://github.com/jojoldu
+ */
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+@ConditionalOnProperty(name = "test.type", havingValue = "3")
+public class WebMvcConfig3 {
+
+    private final ObjectMapper objectMapper;
+
+    @Bean
+    public MappingJackson2HttpMessageConverter jsonEscapeConverter() {
+        ObjectMapper copy = objectMapper.copy();
+        copy.getFactory().setCharacterEscapes(new HtmlCharacterEscapes());
+        return new MappingJackson2HttpMessageConverter(copy);
+    }
+}
