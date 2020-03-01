2fc47f26584d5a224755774f21fbbd0e76acfc6d
이동욱 <jojoldu@gmail.com>
2020-03-01 21:06:19 +0900

    디폴트 세팅

---
+++ spring-boot-dynamodb/src/main/java/com/jojoldu/blogcode/dynamodb/SpringBootDynamodbApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.blogcode.dynamodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDynamodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDynamodbApplication.class, args);
+    }
+
+}
---
+++ spring-boot-dynamodb/src/test/java/com/jojoldu/blogcode/dynamodb/SpringBootDynamodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.blogcode.dynamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDynamodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
