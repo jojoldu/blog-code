aaedc7f806f42e90b7e1785e7822e0fc6b14a014
이동욱 <jojoldu@gmail.com>
2020-05-30 23:41:00 +0900

    으 공개해도 되나 말아야되나

--- spring-batch-tips/src/main/java/com/jojoldu/blogcode/batch/config/DataSourceConfiguration.java
+++ spring-batch-tips/src/main/java/com/jojoldu/blogcode/batch/config/DataSourceConfiguration.java
@@ -2,21 +2,12 @@ package com.jojoldu.blogcode.batch.config;
 
 import com.zaxxer.hikari.HikariDataSource;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateProperties;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateSettings;
-import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.persistenceunit.PersistenceUnitManager;
-import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import javax.sql.DataSource;
 
