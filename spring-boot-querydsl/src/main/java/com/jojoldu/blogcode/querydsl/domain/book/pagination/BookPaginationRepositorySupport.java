d99d8ed85bd4c1f1ff1157be8564875826541bbd
이동욱 <jojoldu@gmail.com>
2020-10-28 23:11:03 +0900

    페이지 번호가 최대값을 초과할 경우 대비

--- spring-boot-querydsl/src/main/java/com/jojoldu/blogcode/querydsl/domain/book/pagination/BookPaginationRepositorySupport.java
+++ spring-boot-querydsl/src/main/java/com/jojoldu/blogcode/querydsl/domain/book/pagination/BookPaginationRepositorySupport.java
@@ -3,11 +3,14 @@ package com.jojoldu.blogcode.querydsl.domain.book.pagination;
 import com.jojoldu.blogcode.querydsl.domain.book.Book;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.JPQLQuery;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.Querydsl;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
 
@@ -32,30 +35,7 @@ public class BookPaginationRepositorySupport extends QuerydslRepositorySupport {
     }
 
     public Page<BookPaginationDto> paginationCount(Pageable pageable, String name) {
-        JPQLQuery<BookPaginationDto> query = Objects.requireNonNull(getQuerydsl()).applyPagination(pageable,
-                queryFactory
-                .select(Projections.fields(BookPaginationDto.class,
-                        book.id.as("bookId"),
-                        book.name,
-                        book.bookNo,
-                        book.bookType
-                ))
-                .from(book)
-                .where(
-                        book.name.like(name + "%")
-                )
-                .orderBy(book.id.desc()));
-
-        List<BookPaginationDto> items = query.fetch(); // 데이터 조회
-        long totalCount = query.fetchCount(); // 전체 count
-        return new PageImpl<>(items, pageable, totalCount);
-    }
-
-    /**
-     * 3-1. 검색 버튼사용 count 고정값 쓰기
-     */
-    public Page<BookPaginationDto> paginationCountSearchBtn(boolean useSearchBtn, Pageable pageable, String name) {
-        JPQLQuery<BookPaginationDto> query = Objects.requireNonNull(getQuerydsl()).applyPagination(pageable,
+        JPQLQuery<BookPaginationDto> query = querydsl().applyPagination(pageable,
                 queryFactory
                         .select(Projections.fields(BookPaginationDto.class,
                                 book.id.as("bookId"),
@@ -70,13 +50,86 @@ public class BookPaginationRepositorySupport extends QuerydslRepositorySupport {
                         .orderBy(book.id.desc()));
 
         List<BookPaginationDto> items = query.fetch(); // 데이터 조회
+        long totalCount = query.fetchCount(); // 전체 count
+        return new PageImpl<>(items, pageable, totalCount);
+    }
+
+    /**
+     * 3-1. 검색 버튼사용 count 고정값 쓰기
+     */
+    public Page<BookPaginationDto> paginationCountSearchBtn(boolean useSearchBtn, Pageable pageable, String name) {
+        JPAQuery<BookPaginationDto> query = queryFactory
+                .select(Projections.fields(BookPaginationDto.class,
+                        book.id.as("bookId"),
+                        book.name,
+                        book.bookNo,
+                        book.bookType
+                ))
+                .from(book)
+                .where(
+                        book.name.like(name + "%")
+                )
+                .orderBy(book.id.desc());
+
+        JPQLQuery<BookPaginationDto> pagination = querydsl().applyPagination(pageable, query);
+
+        if(useSearchBtn) {
+            int fixedPageCount = 10 * pageable.getPageSize(); // 10개 페이지 고정
+            return new PageImpl<>(pagination.fetch(), pageable, fixedPageCount);
+        }
+
+        long totalCount = pagination.fetchCount();
+        Pageable pageRequest = exchangePageRequest(pageable, totalCount);
+        return new PageImpl<>(querydsl().applyPagination(pageRequest, query).fetch(), pageRequest, totalCount);
+    }
+
+    Pageable exchangePageRequest(Pageable pageable, long totalCount) {
 
         /**
-         *  검색 버튼을 사용했을 경우: 10 페이지 건수 , 일반 페이지 버튼을 클릭했을 경우 실제 건수
+         *  요청한 페이지 번호가 기존 데이터 사이즈를 초과할 경우
+         *  마지막 페이지의 데이터를 반환한다
          */
-        int tempPageCount = 10 * pageable.getPageSize(); // 10개 페이지 * 페이지당 건수
-        long totalCount = useSearchBtn? tempPageCount : query.fetchCount();
-        return new PageImpl<>(items, pageable, totalCount);
+        int pageNo = pageable.getPageNumber();
+        int pageSize = pageable.getPageSize();
+        long requestCount = (pageNo - 1) * pageSize; // pageNo:10, pageSize:10 일 경우 requestCount=90
+
+        if (totalCount > requestCount) { //
+            return pageable;
+        }
+
+        int requestPageNo = (int) totalCount / pageSize;
+        return PageRequest.of(requestPageNo, pageSize);
+
+    }
+
+    public Page<BookPaginationDto> paginationCountSearchBtn2(boolean useSearchBtn, Pageable pageable, String name) {
+        JPAQuery<BookPaginationDto> query = queryFactory
+                .select(Projections.fields(BookPaginationDto.class,
+                        book.id.as("bookId"),
+                        book.name,
+                        book.bookNo,
+                        book.bookType
+                ))
+                .from(book)
+                .where(
+                        book.name.like(name + "%")
+                )
+                .orderBy(book.id.desc());
+
+        JPQLQuery<BookPaginationDto> pagination = querydsl().applyPagination(pageable, query);
+
+        if(useSearchBtn) {
+            int fixedPageCount = 10 * pageable.getPageSize(); // 10개 페이지 고정
+            return new PageImpl<>(pagination.fetch(), pageable, fixedPageCount);
+        }
+
+        long totalCount = pagination.fetchCount();
+        Pageable pageRequest = new FixedPageRequest(pageable, totalCount);
+        return new PageImpl<>(querydsl().applyPagination(pageRequest, query).fetch(), pageRequest, totalCount);
+    }
+
+    Querydsl querydsl() {
+        return Objects.requireNonNull(getQuerydsl());
     }
 
 
@@ -84,7 +137,7 @@ public class BookPaginationRepositorySupport extends QuerydslRepositorySupport {
      * 3-2. 첫 페이지 조회 결과 cache 하기
      */
     public Page<BookPaginationDto> paginationCountCache(Long cachedCount, Pageable pageable, String name) {
-        JPQLQuery<BookPaginationDto> query = Objects.requireNonNull(getQuerydsl()).applyPagination(pageable,
+        JPQLQuery<BookPaginationDto> query = querydsl().applyPagination(pageable,
                 queryFactory
                         .select(Projections.fields(BookPaginationDto.class,
                                 book.id.as("bookId"),
@@ -99,7 +152,7 @@ public class BookPaginationRepositorySupport extends QuerydslRepositorySupport {
                         .orderBy(book.id.desc()));
 
         List<BookPaginationDto> elements = query.fetch(); // 데이터 조회
-        long totalCount = cachedCount!=null? cachedCount: query.fetchCount(); // 전체 count
+        long totalCount = cachedCount != null ? cachedCount : query.fetchCount(); // 전체 count
         return new PageImpl<>(elements, pageable, totalCount);
     }
 
