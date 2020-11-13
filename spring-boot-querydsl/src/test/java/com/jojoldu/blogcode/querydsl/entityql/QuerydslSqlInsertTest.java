be3a6e3bebf49b3e483adf49e72b8938337de507
이동욱 <jojoldu@gmail.com>
2020-11-13 21:23:19 +0900

    entityql 설정 실패 수정

--- spring-boot-querydsl/src/test/java/com/jojoldu/blogcode/querydsl/entityql/QuerydslSqlInsertTest.java
+++ spring-boot-querydsl/src/test/java/com/jojoldu/blogcode/querydsl/entityql/QuerydslSqlInsertTest.java
@@ -1,14 +1,19 @@
 package com.jojoldu.blogcode.querydsl.entityql;
 
 import com.jojoldu.blogcode.querydsl.config.EntityMapper;
+import com.jojoldu.blogcode.querydsl.domain.academy.Academy;
+import com.jojoldu.blogcode.querydsl.domain.academy.AcademyRepository;
 import com.jojoldu.blogcode.querydsl.domain.book.Book;
 import com.jojoldu.blogcode.querydsl.domain.book.BookRepository;
 import com.jojoldu.blogcode.querydsl.domain.book.BookType;
 import com.jojoldu.blogcode.querydsl.domain.pointevent.PointEvent;
 import com.jojoldu.blogcode.querydsl.domain.pointevent.PointEventRepository;
 import com.jojoldu.blogcode.querydsl.domain.pointevent.PointStatus;
+import com.jojoldu.blogcode.querydsl.domain.student.Student;
+import com.jojoldu.blogcode.querydsl.domain.student.StudentRepository;
 import com.querydsl.sql.SQLQueryFactory;
 import com.querydsl.sql.dml.BeanMapper;
+import com.querydsl.sql.dml.SQLInsertClause;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,10 +21,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static com.jojoldu.blogcode.querydsl.domain.sql.EBook.qBook;
 import static com.jojoldu.blogcode.querydsl.domain.sql.EPointEvent.qPointEvent;
+import static com.jojoldu.blogcode.querydsl.domain.sql.EStudent.qStudent;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -41,10 +48,17 @@ public class QuerydslSqlInsertTest {
     @Autowired
     private BookRepository bookRepository;
 
+    @Autowired
+    private AcademyRepository academyRepository;
+
+    @Autowired
+    private StudentRepository studentRepository;
+
     @AfterEach
     void after() {
         pointEventRepository.deleteAllInBatch();
         bookRepository.deleteAllInBatch();
+        academyRepository.deleteAll();
     }
 
     @Test
@@ -90,5 +104,31 @@ public class QuerydslSqlInsertTest {
         assertThat(results).hasSize(1);
         assertThat(results.get(0).getId()).isEqualTo(1L);
         assertThat(results.get(0).getBookType()).isEqualTo(BookType.IT);
+    }
+
+    @Test
+    void sqlOneToManyInsert() throws Exception {
+        //given
+        String academyName = "name1";
+        Academy academy = academyRepository.save(Academy.builder()
+                .address("address1")
+                .name(academyName)
+                .build());
+
+        Student student1 = new Student("student1", 1);
+        Student student2 = new Student("student2", 2);
+        academy.addStudent(Arrays.asList(student1, student2));
+
+        //when
+        SQLInsertClause insert = sqlQueryFactory.insert(qStudent);
+        insert.populate(student1, EntityMapper.DEFAULT).addBatch();
+        insert.populate(student2, EntityMapper.DEFAULT).addBatch();
+        insert.execute();
+
+        //then
+        List<Student> students = studentRepository.findAll();
+        assertThat(students).hasSize(2);
+        assertThat(students.get(0).getAcademy().getId()).isEqualTo(academy.getId());
+        assertThat(students.get(1).getAcademy().getId()).isEqualTo(academy.getId());
     }
 }
