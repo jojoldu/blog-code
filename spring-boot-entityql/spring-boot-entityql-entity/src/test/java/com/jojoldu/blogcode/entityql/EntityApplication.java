9cf2c0a26770bbd62bfaa2717e9d2afc7cb35fb5
jojoldu <jojoldu@gmail.com>
2021-02-27 20:02:19 +0900

    entityql 멀티모듈로 새롭게 진행

---
+++ spring-boot-entityql/spring-boot-entityql-entity/src/test/java/com/jojoldu/blogcode/entityql/EntityApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.blogcode.entityql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class EntityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntityApplication.class, args);
+    }
+
+}
---
+++ spring-boot-entityql/spring-boot-entityql-sql/src/test/java/com/jojoldu/blogcode/entityql/SqlApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.blogcode.entityql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class SqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlApplication.class, args);
+    }
+
+}
